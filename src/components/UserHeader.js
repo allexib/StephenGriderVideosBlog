63dbbd576000499f3eb8bcd0e79f652e407e7e6a
allexib <ooomegasib@gmail.com>
2021-02-24 21:00:19 +0400

    16\25 275 Extracting Logic to MapStateToProps

--- src/components/UserHeader.js
+++ src/components/UserHeader.js
@@ -2,13 +2,14 @@ import React from 'react';
 import {connect} from 'react-redux';
 import {fetchUser} from "../actions";
 
-class UserHeader  extends React.Component {
+class UserHeader extends React.Component {
     componentDidMount() {
         this.props.fetchUser(this.props.userId);
     }
 
     render() {
-        const user = this.props.users.find((user) => user.id === this.props.userId);
+        const {user} = this.props;
+       // const user = this.props.users.find((user) => user.id === this.props.userId);
 
         if (!user) {
             return null;
@@ -18,8 +19,11 @@ class UserHeader  extends React.Component {
     }
 }
 
-const mapStateToProps = (state) => {
-    return {users: state.users};
+const mapStateToProps = (state, ownProps) => {
+    return {user: state.users.find(user => user.id === ownProps.userId)};
 };
 
-export default connect(mapStateToProps, {fetchUser})(UserHeader);
+export default connect(
+    mapStateToProps,
+    {fetchUser}
+)(UserHeader);
