82afe9e9745e5fa013956e6eb001b4d50478d59a
allexib <ooomegasib@gmail.com>
2021-02-24 17:01:43 +0400

    13\25 272 Fetching Singular Records

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,12 @@ export const fethPosts = () => async dispatch => {
     dispatch({type: 'FETCH_POSTS', payload: response.data})
 };
 
+export const fetchUser = (id) => async dispatch => {
+    const response = await  jsonPlaceholder.get(`/users/${id}`);
+    
+    dispatch({type: 'FETCH_USER', payload: response.data})
+};
+
 
 
 
